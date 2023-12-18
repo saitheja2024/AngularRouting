c72b1fb24eb8e456b54742dc909e9e243334e531
Pankaj Jain <pankaj.jain@itaugments.com>
2023-12-18 11:49:02 -0500

    Routing issue

--- src/app/modules/family/family.module.ts
+++ src/app/modules/family/family.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ChinmayaSharedModule } from '../chinmaya-shared/chinmaya-shared.module';
 import { FamilyRoutingModule } from './family-routing.module';
 import { FamilyComponent } from './components/family/family.component';
+import { AllFamilyMembersComponent } from './components/all-family-members/all-family-members.component';
+import { AllFamiliesComponent } from './components/all-families/all-families.component';
 
 
 
 @NgModule({
-  declarations: [FamilyComponent],
+  declarations: [FamilyComponent,AllFamiliesComponent,AllFamilyMembersComponent],
   imports: [
     FamilyRoutingModule,
     ChinmayaSharedModule
