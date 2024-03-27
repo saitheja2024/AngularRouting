5948de8bc174098123b0317e7f6c2db77c2c83a5
Pankaj Jain <pankaj.jain@itaugments.com>
2024-03-27 02:23:13 -0500

    authentication if member is landed from registartion portal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {TranslationService} from './modules/i18n';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { TranslationService } from './modules/i18n';
 // language list
-import {locale as enLang} from './modules/i18n/vocabs/en';
-import {locale as chLang} from './modules/i18n/vocabs/ch';
-import {locale as esLang} from './modules/i18n/vocabs/es';
-import {locale as jpLang} from './modules/i18n/vocabs/jp';
-import {locale as deLang} from './modules/i18n/vocabs/de';
-import {locale as frLang} from './modules/i18n/vocabs/fr';
-import {ThemeModeService} from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
+import { locale as enLang } from './modules/i18n/vocabs/en';
+import { locale as chLang } from './modules/i18n/vocabs/ch';
+import { locale as esLang } from './modules/i18n/vocabs/es';
+import { locale as jpLang } from './modules/i18n/vocabs/jp';
+import { locale as deLang } from './modules/i18n/vocabs/de';
+import { locale as frLang } from './modules/i18n/vocabs/fr';
+import { ThemeModeService } from './_metronic/partials/layout/theme-mode-switcher/theme-mode.service';
 import { SpinnerService } from './modules/chinmaya-shared/services/spinner/spinner.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from './modules/auth';
@@ -25,9 +25,9 @@ export class AppComponent implements OnInit {
     private translationService: TranslationService,
     private modeService: ThemeModeService,
     public spinnerService: SpinnerService,
-    private activatedRoute:ActivatedRoute,
-    private authService:AuthService,
-    private router:Router
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService,
+    private router: Router
   ) {
     // register translations
     this.translationService.loadTranslations(
@@ -40,18 +40,24 @@ export class AppComponent implements OnInit {
     );
   }
 
-   ngOnInit() {
+  async ngOnInit() {
     this.modeService.init();
-    this.activatedRoute.queryParams.subscribe(async (params:any) => {
-      let userName = params["key"];
-      console.log(userName);
-      if(!userName){
-        this.router.navigateByUrl("/auth");
-        console.log("returning to login page");
-        return;
-      }
-      const user = await this.authService.login({username:userName,password:"123456"});
-      this.router.navigateByUrl("")
-    });
+    console.log(window.location.href);
+    let currentURL = window.location.href;
+
+    // Following condition is used if user landing from member registration portal. 
+    if (currentURL.includes('?')) {
+      const url = new URL(currentURL);
+
+      // Get the value of the 'key' parameter
+      const key = url.searchParams.get('key');
+
+      // Get the value of the 'value' parameter
+      const value = url.searchParams.get('value');
+      const user = await this.authService.login({ username: key, password: value });
+      this.router.navigateByUrl("");
+      return;
+    }
+    this.router.navigateByUrl("/auth");
   }
 }
