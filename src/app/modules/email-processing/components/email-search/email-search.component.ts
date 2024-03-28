942cac21ca7bf59cf997f1e1a76be1414d09f7c5
ravindranath <ravindranath.dobila@itaugments.com>
2024-03-28 13:27:29 +0530

    Pankaj is commiting this code for search result page naviagation

--- src/app/modules/email-processing/components/email-search/email-search.component.ts
+++ src/app/modules/email-processing/components/email-search/email-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-email-search',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class EmailSearchComponent {
 
+
+  constructor(private route:Router){}
+
+  onSearch(){
+    this.route.navigateByUrl("email-processing/email-search-results");
+  }
 }
