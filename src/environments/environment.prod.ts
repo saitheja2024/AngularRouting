dd007d97309faf41db07632e38e41eb680c42053
pankajin15 <133360424+pankajin15@users.noreply.github.com>
2024-04-10 12:12:27 +0530

    logout functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
   apiUrl: 'api',
-  memberRegPortalURL:"https://reg2.cmwrcregistration.org/MemberReg/",
+  memberRegPortalURL:"https://reg2.cmwrcregistration.org/MemberReg",
   appThemeName: 'Metronic',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
   appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
