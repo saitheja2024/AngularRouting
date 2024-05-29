df7273ba1436fb03992bdf254871460e5c9ce69b
harikrishna.g <harikrishna.g@itaugments.com>
2024-05-29 00:06:12 -0400

    build environment configuration

---
+++ src/environments/environment.core.ts
@@ -0,0 +1,48 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: true,
+    appVersion: 'v8.1.8',
+    USERDATA_KEY: 'authf649fc9a5f55',
+    isMockEnabled: true,
+    apiUrl: 'api',
+    memberRegPortalURL:"https://nonregcore.cmwrcregistration.org/MemberReg",
+    appThemeName: 'Metronic',
+    appPurchaseUrl: 'https://1.envato.market/EA4JP',
+    appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
+    appPreviewUrl: 'https://preview.keenthemes.com/metronic8/angular/demo1/',
+    appPreviewAngularUrl: 'https://preview.keenthemes.com/metronic8/angular/demo1',
+    appPreviewDocsUrl: 'https://preview.keenthemes.com/metronic8/angular/docs',
+    appPreviewChangelogUrl: 'https://preview.keenthemes.com/metronic8/angular/docs/changelog',
+    appDemos: {
+      'demo1': {
+        'title': 'Demo 1',
+        'description': 'Default Dashboard',
+        'published': true,
+        'thumbnail': './assets/media/demos/demo1.png'
+      },
+     },
+     baseURL: 'https://nonregcore.cmwrcregistration.org/MR/',
+     loginID:'6pUQ10eF2r',
+     AUTH_TOKEN_STORAGE_KEY: 'token',
+     AUTH_TOKEN_HEADER_NAME: 'X-Auth-Token',
+     API_Access_ID:'d2e81e0ee4f0423ebc3559d694c34a20',
+     API_Secure_Key:'f0fed3faa449ef5b4db7b80957d53734',
+     LOCATION_ID:'221096',
+   
+  
+  
+  
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  
