a6e472f888775553f4b2066fe8c4fa8826312ff1
harikrishna.g <harikrishna.g@itaugments.com>
2024-08-29 22:57:40 -0400

    Validation implemented for m-check payment option

--- src/app/modules/programregistration/components/payment/payment/payment.component.ts
+++ src/app/modules/programregistration/components/payment/payment/payment.component.ts
@@ -361,6 +361,7 @@ if(arr!=null){
 
   cardValueOpt(){
   this.ccCardPay='';
+  this.PaymCheck='';
   }
 
   completePaymentNow(){
@@ -522,6 +523,9 @@ if(arr!=null){
   }
 
 
+  allowNummericsOnly(event:any){
+    event.target.value = event.target.value.replace(/[^0-9]*/g, '');
+  }
 }
 
 
